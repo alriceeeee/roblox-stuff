a46bd34b0749af2d33d544fa91fd32e2791f5c94
alrice <isaiahlowder06@outlook.com>
2024-12-25 16:48:18 -0600

    Create pets.lua

---
+++ pets.lua
@@ -0,0 +1,21 @@
+return {
+    "Purple Dog", "Green Golem", "Crimson Vampy", "Orange Pegasus", "Purple Dragon",
+    "Red Pheonix", "Red Kitty", "Yellow Sqeak", "Orange Hedgehog", "Blue Birdie",
+    "Green Bunny", "Dark Golem", "Red Bunny", "Green Butterfly", "Blue Falcon",
+    "Light Angel", "Orange Dragon", "Blue Pheonix", "Diamond Strike Falcon",
+    "Dark Karma Dragon", "Corrupted Soul Dragon", "Mini Sensei", "Inner Peace Birdie",
+    "Ultimate Overdrive Bunny", "Orange Falcon", "Mini Ninja", "Blizzard Bunny",
+    "Tundra Dragon", "Quantum Dragon", "Dark Soul Birdie", "Blue Firecaster",
+    "White Pheonix", "Void Dragon", "Electro Bunny", "Magic Butterfly", "Ultra Birdie",
+    "Pink Butterfly", "Orange Falcon", "Purple Pegasus", "Golden Pheonix",
+    "Green Firecaster", "Electro Golem", "Red Dragon", "Voltaic Falcon",
+    "Red Firecaster", "Magical Pegasus", "Flaming Hedgehog", "Infernal Dragon",
+    "Void Omega Pegasus", "Twin Element Birdies", "Orange Birdie", "Tan Dog",
+    "Red Golem", "Yellow Butterfly", "Purple Falcon", "White Pegasus",
+    "Shadowstorm Bunny", "Inner Peace Legion", "Starhunter Pegasus",
+    "Butterfly Sensei", "Eternal Nebula Dragon", "Soul Clash Golem",
+    "Soul Fusion Dog", "Supercharged Midnight Kitty", "Thunder Strike Falcon",
+    "Dawn Horizon Birdie", "Eternal Abyss Wyvern", "Rising Divine Dragon",
+    "Golden Spirit Kitty", "Mini Legend", "Eternal Strike Leviathan",
+    "Mystical Midnight Pegasus", "Swift Eclipse Bunny"
+}
